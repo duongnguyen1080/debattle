3a19cd1bf2d83f43893ca65eb093fec75e808ed0
Duong Nguyen <duongthuynguyen118@gmail.com>
2026-01-07 10:11:23 +0700

    Updated assets

--- src/components/home/uiConstants.ts
+++ src/components/home/uiConstants.ts
@@ -2,4 +2,4 @@ export const CTA_BUTTON_WIDTH_PX = 203;
 export const CTA_BUTTON_HEIGHT_PX = 106;
 export const CTA_BOTTOM_INSET_PX = 56;
 
-export const NAV_ICON_SIZE_PX = 44;
+export const NAV_ICON_SIZE_PX = 55;
